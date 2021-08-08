cb04864502901aaacef6197215efd607f19e515d
aike-s <sagrilo804@gmail.com>
2021-08-08 18:48:53 -0500

    [Update] method all for handle return objects of a specific class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            obj_list = {}
+            for key, value in FileStorage.__objects.items():
+                if cls == value.__class__:
+                    obj_list[key] = value
+            return(obj_list)
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +32,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ Delete an obj from __objects """
+        # fs.delete(new_state)
+        if obj is not None:
+            key = (type(obj).__name__ + '.' + obj.id)
+            del(self.all()[key])
 
     def reload(self):
         """Loads storage dictionary from file"""
