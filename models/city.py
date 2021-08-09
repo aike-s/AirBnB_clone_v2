ae2b280876c4586f53cde71aa8c3d1f173101529
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-08-09 16:41:40 -0500

    Fixing city and state

--- models/city.py
+++ models/city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy.sql.schema import Column, ForeignKey
-from sqlalchemy.sql.sqltypes import String
+from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
+
     name = Column(String(128),
                   nullable=False)
 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from AirBnB_clone_v2.models.city import City
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128),
+                  nullable=False)
+
+    if getenv("HBNB_MYSQL_DB") == "db":
+        cities = relationship(
+            "City", cascade="all, delete-orphan", backref="state")
+
+    else:
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id"""
+            cities_list = []
+            all_cities = models.storage.all(City)
+            
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+
+            return cities_list
