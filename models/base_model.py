d524eca92dc8e43d21477722f7228637d82b025d
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-08-09 14:24:24 -0500

    fixing basemodel task 6

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,13 @@ from datetime import date, datetime
 
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import models
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
-
     id = Column(String(60),
                 unique=True,
                 primary_key=True)
@@ -22,13 +23,12 @@ class BaseModel:
                         nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""                 
+        """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -46,6 +46,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -57,3 +58,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """Is a new public instance method tha elete the current instance from
+        the storage (models.storage) by calling the method delete"""
+        models.storage.delete(self)
