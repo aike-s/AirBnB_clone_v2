35ca98f5d889fcafa68f028bb273ddf0ab173094
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-08-09 13:20:40 -0500

    Creating declarative base and adding id, create and update

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
+from datetime import date, datetime
 
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60),
+                unique=True,
+                primary_key=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow,
+                        nullable=False)
+
+    updated_at = Column(DateTime, default=datetime.utcnow,
+                        nullable=False)
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instatntiates a new model"""                 
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
