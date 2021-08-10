9c771e5c442fca9917a862ce39e5fa5a2499464d
aike-s <sagrilo804@gmail.com>
2021-08-10 16:17:47 -0500

    [add] class attributes for every class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+from sqlalchemy import Column, String
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ Initialization of table/class User """
+
+    __tablename__ = 'users'
+
+    name = Column(String(128), nullable=False)
