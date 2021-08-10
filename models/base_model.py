e6660b56c5c349100d4998a1858bcf204398dd7a
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-08-10 08:31:01 -0500

    fixing pep8

--- models/base_model.py
+++ models/base_model.py
@@ -41,7 +41,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self) #Mamarracho para ver si se corrige el punto 2
+            storage.new(self)  # Mamarracho para ver si se corrige el punto 2
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,7 +52,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new(self) # Acá esto se movio pero no se muy bien que hace
+        storage.new(self)  # Acá esto se movio pero no se muy bien que hace
         storage.save()
 
     def to_dict(self):
