9eb40f31d8366157a7261fbcced4078059fa5b9d
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-08-10 18:22:28 -0500

    fixing task 8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -62,7 +62,7 @@ class DBStorage:
             data += self.__session.query(State).all()
             data += self.__session.query(City).all()
             # data += self.__session.query(Amenity).all()
-            # data += self.__session.query(Place).all()
+            data += self.__session.query(Place).all()
             # data += self.__session.query(Review).all()
             all_objs = {}
 
