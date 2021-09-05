9f4cd3101cf5f69501ed7f97ffe14a76de4ecdde
aike-s <sagrilo804@gmail.com>
2021-09-05 14:39:05 -0500

    [add] sudo to all commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 # Set up my web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 
 # Create the folders if they doesn't exist
 
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create fake HTML file
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>">/data/web_static/releases/test/index.html
+</html>"> | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symlink
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Config nginx to serve the content of /data/web_static/current/ to hbnb_static
-search="server_name _;"
-add_config="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "/$search/a\\$add_config" /etc/nginx/sites-available/default
+sudo search="server_name _;"
+sudo add_config="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "/$search/a\\$add_config" /etc/nginx/sites-available/default
 
-service nginx restart
+sudo service nginx restart
